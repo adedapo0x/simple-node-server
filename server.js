87eb3fff08263de9095a40f8b42fdf28cc187c95
Adeyemi Adedapo <138253834+adedapo0x@users.noreply.github.com>
2024-05-08 17:04:54 +0100

    OS info display implementation complete

--- server.js
+++ server.js
@@ -9,11 +9,12 @@ const server = http.createServer((req, res) => {
 
     // Generating random delay
     const randomTime = Math.floor(Math.random() * (5000 - 2000 + 1) + 2000)
+    
     setTimeout(() =>{
         res.end(JSON.stringify(os.cpus()[0]))
     }, randomTime)
 })
 
 server.listen(3000, ()=>{
-    console.log("Server running quite alright")
+    console.log("Server running quite alright!")
 })
