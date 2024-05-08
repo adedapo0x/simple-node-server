b5bd30073d15bd328dc5c6e0f42e9a0e93f6e5c3
Adeyemi Adedapo <adedapo025@gmail.com>
2024-05-08 16:51:32 +0100

    OS info display implementation completed

--- server.js
+++ server.js
@@ -3,7 +3,16 @@ const os = require('os')
 
 const server = http.createServer((req, res) => {
     // Handling CORS
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000')
 
+    res.writeHead(200, {'Content-Type': 'application/json'})
+
+    // F
+    // Generating random delay
+    const randomTime = Math.floor(Math.random() * (5000 - 2000 + 1) + 2000)
+    setTimeout(() =>{
+        res.end(JSON.stringify(os.cpus()[0]))
+    }, randomTime)
 })
 
 server.listen(3000, ()=>{
